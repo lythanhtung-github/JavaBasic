fea9d2bc5aa7f4d96b03dc98a6dfa0a27f2bd79d
Lý Thanh Tùng <lythanhtung.work@gmail.com>
2022-08-10 15:12:14 +0700

    hien thi so nguyen to be hon 100

---
+++ hien-thi-so-nguyen-to-100/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("Các số nguyên tố bé hơn 100");
+        int count = 0;
+        for (int num = 2; num < 100; num++ ){
+            for (int i = 1; i <= num; i++){
+                if (num % i == 0 ){
+                    ++count;
+                }
+            }
+            if (count == 2){
+                System.out.print(num + " ");
+            }
+            count = 0;
+        }
+    }
+}
