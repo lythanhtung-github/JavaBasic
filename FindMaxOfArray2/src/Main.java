b71df0864f698e558bab312a74926953f8464894
Lý Thanh Tùng <lythanhtung.work@gmail.com>
2022-08-12 08:30:58 +0700

    tim max mang 2 chieu

---
+++ FindMaxOfArray2/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int row, col;
+
+        do {
+            System.out.println("Nhập số dòng cho mảng: ");
+            row = sc.nextInt();
+            System.out.println("Nhập số cột cho mảng: ");
+            col = sc.nextInt();
+        } while (row < 0 || col < 0);
+
+        int[][] numbers = new int[row][col];
+
+        for (int i = 0; i  < row; i ++){
+            for(int j = 0; j < col; j ++){
+                System.out.printf("Màng phần tử %d - Nhập phần tử thứ %d: ", i, j);
+                numbers[i][j]= sc.nextInt();
+            }
+        }
+
+        int max = numbers[0][0];
+        System.out.println("Mảng 2 chiều: ");
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[i].length; j++) {
+                System.out.printf("%-4d", numbers[i][j]);
+                if (numbers[i][j] > max) {
+                    max = numbers[i][j];
+                }
+            }
+            System.out.println();
+        }
+        System.out.printf("Gíá trị lớn nhất của mảng là %d:", max);
+    }
+}
