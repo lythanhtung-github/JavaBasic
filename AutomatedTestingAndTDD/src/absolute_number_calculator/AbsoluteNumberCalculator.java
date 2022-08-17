d5b17c3237d39e3bee739e38c9b6d2365aeb0ab0
Lý Thanh Tùng <lythanhtung.work@gmail.com>
2022-08-17 22:22:04 +0700

    test abs

---
+++ AutomatedTestingAndTDD/src/absolute_number_calculator/AbsoluteNumberCalculator.java
@@ -0,0 +1,9 @@
+package absolute_number_calculator;
+
+public class AbsoluteNumberCalculator {
+    public static int findAbsolute(int number) {
+        if (number < 0)
+            return -number;
+        return number;
+    }
+}
---
+++ AutomatedTestingAndTDD/src/absolute_number_calculator/AbsoluteNumberCalculatorTest.java
@@ -0,0 +1,33 @@
+package absolute_number_calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AbsoluteNumberCalculatorTest {
+    @Test
+    void testFindAbsolute0() {
+        int number = 0;
+        int expected = 0;
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testFindAbsolute1() {
+        int number = 1;
+        int expected = 1;
+
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testFindAbsoluteNegative1() {
+        int number = -1;
+        int expected = 1;
+
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected, result);
+    }
+}
