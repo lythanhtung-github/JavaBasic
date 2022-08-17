ca77b42728bccb5edba3bee2e932fc3513ad3a8e
Lý Thanh Tùng <lythanhtung.work@gmail.com>
2022-08-17 22:01:53 +0700

    test calculate

---
+++ AutomatedTestingAndTDD/src/simple_calculator/SimpleCalculator.java
@@ -0,0 +1,11 @@
+package simple_calculator;
+
+public class SimpleCalculator {
+    public static int add(int first, int second){
+        return first + second;
+    }
+
+    public static int sub(int first, int second){
+        return first - second;
+    }
+}
