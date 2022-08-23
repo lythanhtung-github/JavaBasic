681963723c4605607e7e78a53b3f09adee434c3e
Lý Thanh Tùng <lythanhtung.work@gmail.com>
2022-08-23 20:28:22 +0700

    reverse

--- ReverseArrayStack/src/ReverseArrayTest.java
+++ ReverseArrayStack/src/ReverseArrayTest.java
@@ -5,11 +5,16 @@ public class ReverseArrayTest {
 
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         System.out.printf("Mảng ban đầu: %s\n", Arrays.toString(numbers));
-        numbers = reverseArray(numbers);
+        numbers = reverseArrayInt(numbers);
         System.out.printf("Mảng đảo ngược: %s\n", Arrays.toString(numbers));
+
+        String str = "Ly Thanh Tung";
+        System.out.printf("Chuỗi ban đầu: %s\n", str);
+        str = reverseArrayString(str);
+        System.out.printf("Chuỗi đảo ngược: %s\n", str);
     }
 
-    public static int[] reverseArray(int[] numbers) {
+    public static int[] reverseArrayInt(int[] numbers) {
         Stack<Integer> stack = new Stack<>();
         for (int i = 0; i < numbers.length; i++) {
             stack.push(numbers[i]);
@@ -18,5 +23,17 @@ public class ReverseArrayTest {
             numbers[i] = stack.pop();
         }
         return numbers;
+    }
+
+    public static String reverseArrayString(String str) {
+        Stack<String> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i) + "");
+        }
+        String newString = "";
+        for (int i = 0; i < str.length(); i++) {
+            newString = newString + stack.pop();
+        }
+        return newString;
     }
 }
