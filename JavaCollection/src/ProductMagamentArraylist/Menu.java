cce762975969549c6b5835dfb69a2e5dd0c5e692
Lý Thanh Tùng <lythanhtung.work@gmail.com>
2022-08-25 07:26:43 +0700

    java collection

---
+++ JavaCollection/src/ProductMagamentArraylist/Menu.java
@@ -0,0 +1,15 @@
+package ProductMagamentArraylist;
+
+public class Menu {
+
+    public void showMenu() {
+        System.out.println("1. Thêm sản phẩm");
+        System.out.println("2. Sửa thông tin theo id");
+        System.out.println("3. Xóa sản phẩm theo id");
+        System.out.println("4. Hiển thị danh sách sản phẩm");
+        System.out.println("5. Tìm kiếm sản phẩm theo tên");
+        System.out.println("6. Sắp xếp sản phẩm tăng dần");
+        System.out.println("7. Sắp xếp sản phẩm giảm dần");
+        System.out.println("8. Thoát");
+    }
+}
