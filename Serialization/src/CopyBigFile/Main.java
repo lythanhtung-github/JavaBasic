8e907c3faf70a043dbe2251fea0c19e214ee9376
Lý Thanh Tùng <lythanhtung.work@gmail.com>
2022-09-04 19:23:43 +0700

    copy big file serialization

---
+++ Serialization/src/CopyBigFile/Main.java
@@ -0,0 +1,53 @@
+package CopyBigFile;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Scanner;
+
+public class Main {
+    private static void copyFileUsingJava7Files(File source, File dest) throws IOException, IOException {
+        Files.copy(source.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    private static void copyFileUsingStream(File source, File dest) throws IOException {
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = new FileInputStream(source);
+            os = new FileOutputStream(dest);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+            }
+        } finally {
+            is.close();
+            os.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.printf("Enter source file:");
+        String sourcePath = in.nextLine();
+        System.out.printf("Enter destination file:");
+        String destPath = in.nextLine();
+
+        File sourceFile = new File(sourcePath);
+        File destFile = new File(destPath);
+
+        try {
+            copyFileUsingJava7Files(sourceFile, destFile);
+            //copyFileUsingStream(sourceFile, destFile);
+            System.out.printf("Copy completed");
+        } catch (IOException ioe) {
+            System.out.printf("Can't copy that file");
+            System.out.printf(ioe.getMessage());
+        }
+    }
+
+    //E:\GitHub\JavaBasic\Serialization\src\CopyBigFile\1.data
+    //E:\GitHub\JavaBasic\Serialization\src\CopyBigFile\2.data
+}
