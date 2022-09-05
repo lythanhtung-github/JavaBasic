2ed6a365cf14ef2b6848309a7d9a2d3369eb8246
Lý Thanh Tùng <lythanhtung.work@gmail.com>
2022-09-05 22:51:01 +0700

    regex

---
+++ String-Regex/src/news/CrawNewsExample.java
@@ -0,0 +1,38 @@
+package news;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CrawNewsExample {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://dantri.com.vn/the-gioi.htm");
+            Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
+            scanner.useDelimiter("\\Z");
+            String content = scanner.next();
+            scanner.close();
+
+            content = content.replaceAll("\\n+", "");
+
+            Pattern p = Pattern.compile("<h3 class=\"article-title\">.*?>(.*?)</a>");
+            Matcher m = p.matcher(content);
+            while (m.find()) {
+                System.out.println(m.group(1));
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
